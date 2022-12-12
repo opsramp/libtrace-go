ec20be08d353ed1fa6bb49ccfeaeed9dcf0de2cd
saikalyan.bhagavathula <saikalyan.bhagavathula@opsramp.com>
2022-12-12 16:11:55 +0530

    auth token generation and renewal for independent trace proxy

--- transmission/transmission.go
+++ transmission/transmission.go
@@ -854,7 +854,8 @@ var grpcInterceptor = func(ctx context.Context,
 	opts ...grpc.CallOption,
 ) error {
 
-	ctx = metadata.NewOutgoingContext(ctx, metadata.New(map[string]string{"Authorization": Opsramptoken}))
+	tokenChecker := fmt.Sprintf("Bearer %s", Opsramptoken)
+	ctx = metadata.NewOutgoingContext(ctx, metadata.New(map[string]string{"Authorization": tokenChecker}))
 	err := invoker(ctx, method, req, reply, cc, opts...)
 	if status.Code(err) == codes.Unauthenticated {
 		// renew oauth token here before retry
